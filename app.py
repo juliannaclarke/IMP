0adf6baa5690ff026e9479a5e198dc9e33513822
Eric Lacey <34018178+EricLacey@users.noreply.github.com>
2020-12-02 22:34:30 -0500

    Added basic GUI
    Toolbar w buttons for each function
    File Menu w Save, Load, Exit

--- app.py
+++ app.py
@@ -15,15 +15,39 @@ class MainApp(tk.Frame):
 
         menuBar = tk.Menu(self.master)
         self.fileMenu = tk.Menu(self.master, tearoff=0)
+        self.fileMenu.add_command(label="Save", command=self.onSave)
+        self.fileMenu.add_command(label="Load", command=self.onLoad)
         self.fileMenu.add_command(label="Exit", command=self.onExit)
         menuBar.add_cascade(label="File", menu=self.fileMenu)
 
         toolbar = tk.Frame(self.master, bd = 1, relief = tk.RAISED)
 
+        btn_FindEdges = tk.Button(toolbar, text = "Find Edges")
+        btn_SharpenBlur = tk.Button(toolbar, text = "Sharpen/Blur")
+        btn_ColourEdit = tk.Button(toolbar, text = "Colour Manupulation")
+        btn_BrigtContr = tk.Button(toolbar, text = "Brightness/Contrast")
+        btn_VisualEffect = tk.Button(toolbar, text = "VFX")
+
+        btn_FindEdges.pack(side=tk.LEFT, padx=2, pady=2)
+        btn_SharpenBlur.pack(side=tk.LEFT, padx=2, pady=2)
+        btn_ColourEdit.pack(side=tk.LEFT, padx=2, pady=2)
+        btn_BrigtContr.pack(side=tk.LEFT, padx=2, pady=2)
+        btn_VisualEffect.pack(side=tk.LEFT, padx=2, pady=2)
+
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+        self.master.config(menu=menuBar)
+        self.pack()
+
 
     def create_Canvas(self):
         self.canvas = tk.Canvas(root, height=700, width=700, bg="#deadbf")
         self.canvas.pack()
+
+    def onSave(self):
+        self.quit()
+
+    def onLoad(self):
+        self.quit()
 
     def onExit(self):
         self.quit()
